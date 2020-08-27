429299aafd79692eb0ee9a8b4d11bfc10469a5b3
Nakul Ramanathan <nakul16168@iiitd.ac.in>
2020-08-27 11:33:52 +0530

    Final Update on checker.cpp

--- checker.cpp
+++ checker.cpp
@@ -2,18 +2,17 @@
 
 
 bool bpmIsOk(float bpm){
-    return !(bpm < 70 || bpm > 150) ;
+    return (bpm >= 70 && bpm <= 150) ;
 }
 bool spo2IsOk(float spo2){
-    return !(spo2 < 90) ;
+    return (spo2 > 90) ;
 }
 bool respRateIsOk(float respRate){
-    return !(respRate < 30 || respRate > 95);
+    return (respRate >= 30 && respRate <= 95);
 }
 bool vitalsAreOk(float bpm, float spo2, float respRate) {
-  return (bpmIsOk(bpm) || spo2IsOk(spo2) || respRateIsOk(respRate));
+  return (bpmIsOk(bpm) && spo2IsOk(spo2) && respRateIsOk(respRate));
 }
-
 
 int main() {
   assert(vitalsAreOk(80, 95, 60) == true);
