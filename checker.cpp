c4676f7a6971476d5dff9344e268446fcd88a918
Nakul Ramanathan <nakul16168@iiitd.ac.in>
2020-08-27 12:00:42 +0530

    Added Abstraction and Complete testing

--- checker.cpp
+++ checker.cpp
@@ -1,6 +1,8 @@
 #include <assert.h>
 
-
+bool vitalsIsInLimit(float value, int lowerLimit, int upperLimit){
+    return (value >= lowerLimit && value <= upperLimit) ;
+}
 bool bpmIsOk(float bpm){
     return (bpm >= 70 && bpm <= 150) ;
 }
@@ -11,10 +13,18 @@ bool respRateIsOk(float respRate){
     return (respRate >= 30 && respRate <= 95);
 }
 bool vitalsAreOk(float bpm, float spo2, float respRate) {
-  return (bpmIsOk(bpm) && spo2IsOk(spo2) && respRateIsOk(respRate));
+  return (vitalsIsInLimit(bpm , 70 , 150) && vitalsIsInLimit(spo2 , 90 , 100) && vitalsIsInLimit(respRate , 30 , 95));
 }
 
 int main() {
   assert(vitalsAreOk(80, 95, 60) == true);
   assert(vitalsAreOk(60, 90, 40) == false);
+  assert(vitalsAreOk(60, 90, 40) == false);
+  assert(vitalsAreOk(160, 90, 40) == false);
+  assert(vitalsAreOk(80, 90, 40) == true);
+  assert(vitalsAreOk(80, 80, 40) == false);
+  assert(vitalsAreOk(80, 95, 20) == false);
+  assert(vitalsAreOk(80, 95, 40) == true);
+
+  
 }
